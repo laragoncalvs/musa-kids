dc489152e1e4a60d3774cfa7f49e44718e3a2684
laragoncalvs <gs_lara@yahoo.com>
2026-09-15 18:19:51 -0300

    nova musica

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/musa-kids/',
+  base: process.env.NODE_ENV === 'production' ? '/musa-kids/' : '/',
   build: {
     rollupOptions: {
       input: {
